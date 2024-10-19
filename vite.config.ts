871782ca1c54c95f2659bf9533fd2343430f65dc
Luis David Moreno <11556208+luisdamora@users.noreply.github.com>
2024-10-19 17:35:30 +0000

    avance

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'sb1-uuxk3t',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
