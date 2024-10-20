bac364019f08a33aafc274906b3a319e49006265
luis david <11556208+luisdamora@users.noreply.github.com>
2024-10-19 22:27:41 -0500

    feat: Actualizar package.json y gen.ts
    - Se agregó la dependencia "@vercel/node" en package.json.
    - Se transformó el módulo de gen.ts a un export default usando tipos de Vercel.

--- src/api/gen.ts
+++ src/api/gen.ts
@@ -1,4 +1,8 @@
-module.exports = (req, res) => {
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = "World" } = req.query;
-  res.status(200).send(`Hello, ${name}!`);
-};
+  return res.json({
+    message: `Hello ${name}!`,
+  });
+}
