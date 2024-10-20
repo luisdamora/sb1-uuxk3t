e65c8f233f1208d2d1844eddf06499830b77dacd
luis david <11556208+luisdamora@users.noreply.github.com>
2024-10-20 08:59:22 -0500

    feat: Agregar nuevas funcionalidades al proyecto
    - Se añadió la interfaz `IProducto` para definir el modelo del producto.
    - Se incorporó la función `generateUUID` para generar identificadores únicos.
    - Se actualizó el archivo `package.json` para incluir la dependencia `uuid`.

---
+++ src/interfaces/IProducto.ts
@@ -0,0 +1,9 @@
+export interface IProducto {
+  identificador: string;
+  nombre: string;
+  email: string;
+  celular: string;
+  precio_unidad: number;
+  cantidad: number;
+  precio_total: number;
+}
