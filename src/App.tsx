840bd0abe4b13299c69f2623f2fbf3f78e021dac
luis david <11556208+luisdamora@users.noreply.github.com>
2024-10-19 22:23:10 -0500

    feat: Refactorizar formulario en App.tsx
    - Se eliminó el formulario de reserva y se integró el componente `FormReserva`.
    - Se añadió el nuevo archivo `gen.ts` para manejar peticiones API.

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import imagenCancionClaridad from "@assets/images/musicos/cancion_claridad.jpg";
 import imagenCancionSelva from "@assets/images/musicos/cancion_la_selva.jpg";
 import imagenCancionSemilla from "@assets/images/musicos/cancion_la_semilla.jpg";
 
+import { FormReserva } from "@/components/ui2/FormReserva.tsx";
 import videoHeader from "@assets/videos/1926295-hd_1920_1080_25fps_tiny.mp4";
 import { Instagram, MapPin, Youtube } from "lucide-react";
 import { useCallback, useMemo, useRef, useState } from "react";
@@ -160,57 +161,7 @@ function App() {
         id="section-reserva"
         style={{ display: openSectionReserva ? "block" : "none" }}
       >
-        <div className="container mx-auto px-4 py-10 bg-cyan-50 bg-opacity-10 rounded-lg shadow-lg w-full lg:w-1/3">
-          <h2 className="mb-8 text-center text-3xl font-bold text-[#D4AF37]">
-            Reserva tu lugar
-          </h2>
-          <form className="max-w-md mx-auto">
-            <div className="mb-4">
-              <label htmlFor="nombre" className="block mb-2 text-lg">
-                Nombre
-              </label>
-              <input
-                type="text"
-                id="nombre"
-                name="nombre"
-                className="w-full px-3 py-2 text-[#0A1A2A] bg-white border border-[#40E0D0] rounded-md focus:outline-none focus:ring-2 focus:ring-[#D4AF37]"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="email" className="block mb-2 text-lg">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full px-3 py-2 text-[#0A1A2A] bg-white border border-[#40E0D0] rounded-md focus:outline-none focus:ring-2 focus:ring-[#D4AF37]"
-                required
-              />
-            </div>
-            <div className="mb-6">
-              <label htmlFor="celular" className="block mb-2 text-lg">
-                Celular
-              </label>
-              <input
-                type="tel"
-                id="celular"
-                name="celular"
-                className="w-full px-3 py-2 text-[#0A1A2A] bg-white border border-[#40E0D0] rounded-md focus:outline-none focus:ring-2 focus:ring-[#D4AF37]"
-                required
-              />
-            </div>
-            <div className="flex justify-center">
-              <Button
-                type="submit"
-                className="bg-[#D4AF37] text-[#0A1A2A] hover:bg-[#40E0D0] py-4 px-8 text-xl"
-              >
-                Ir a pagar
-              </Button>
-            </div>
-          </form>
-        </div>
+        <FormReserva />
       </section>
 
       {/* Música Medicina y Consciente */}
---
+++ src/components/ui2/FormReserva.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button.tsx";
+import React, { FC } from "react";
+
+interface Props {}
+
+export const FormReserva: FC<Props> = ({}): React.ReactNode => {
+  return (
+    <div className="container mx-auto px-4 py-10 bg-cyan-50 bg-opacity-10 rounded-lg shadow-lg w-full lg:w-1/3">
+      <h2 className="mb-8 text-center text-3xl font-bold text-[#D4AF37]">
+        Reserva tu lugar
+      </h2>
+      <form className="max-w-md mx-auto">
+        <div className="mb-4">
+          <label htmlFor="nombre" className="block mb-2 text-lg">
+            Nombre
+          </label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            className="w-full px-3 py-2 text-[#0A1A2A] bg-white border border-[#40E0D0] rounded-md focus:outline-none focus:ring-2 focus:ring-[#D4AF37]"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block mb-2 text-lg">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full px-3 py-2 text-[#0A1A2A] bg-white border border-[#40E0D0] rounded-md focus:outline-none focus:ring-2 focus:ring-[#D4AF37]"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="celular" className="block mb-2 text-lg">
+            Celular
+          </label>
+          <input
+            type="tel"
+            id="celular"
+            name="celular"
+            className="w-full px-3 py-2 text-[#0A1A2A] bg-white border border-[#40E0D0] rounded-md focus:outline-none focus:ring-2 focus:ring-[#D4AF37]"
+            required
+          />
+        </div>
+        <div className="flex justify-center">
+          <Button
+            type="submit"
+            className="bg-[#D4AF37] text-[#0A1A2A] hover:bg-[#40E0D0] py-4 px-8 text-xl"
+          >
+            Ir a pagar
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
