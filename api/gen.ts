95d8a1a0b2b7a79c7be3742540948aefd666dcb8
luis david <11556208+luisdamora@users.noreply.github.com>
2024-10-19 22:43:30 -0500

    fix: Corregir nombre de la función en gen.ts
    - Se modificó el nombre de la función de exportación a "handler".

--- api/gen.ts
+++ api/gen.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default function (req: VercelRequest, res: VercelResponse) {
+export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = "World" } = req.query;
   return res.json({
     message: `Hello ${name}!`,
