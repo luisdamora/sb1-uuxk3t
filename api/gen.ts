a3803ad7f1f72d3467281697eb087243c52ec1eb
luis david <11556208+luisdamora@users.noreply.github.com>
2024-10-19 23:00:33 -0500

    feat: Actualizar archivos de configuración y dependencias
    - Se actualizó la configuración en gen.ts para utilizar Edge Functions.
    - Se añadió "vite-plugin-vercel" a las dependencias y se modificó la configuración de Vite para incluirlo.

--- api/gen.ts
+++ api/gen.ts
@@ -1,8 +1,16 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
+export const config = {
+  runtime: "edge",
+};
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = "World" } = req.query;
-  return res.json({
-    message: `Hello ${name}!`,
-  });
-}
+export default (request: Request) => {
+  return new Response(`Hello, from ${request.url} I'm now an Edge Function!`);
+};
+
+// import type { VercelRequest, VercelResponse } from "@vercel/node";
+//
+// export default function handler(req: VercelRequest, res: VercelResponse) {
+//   const { name = "World" } = req.query;
+//   return res.json({
+//     message: `Hello ${name}!`,
+//   });
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 import { customPluginCopyAssets } from "./customPluginCopyAssets.ts";
 
 export default defineConfig({
-  plugins: [react(), customPluginCopyAssets()],
+  plugins: [react(), customPluginCopyAssets(), vercel()],
   // base: "sb1-uuxk3t",
   resolve: {
     alias: {
