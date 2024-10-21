2ec414339fafe2e6712ae6c6068f6be7a30a82f4
luis david <11556208+luisdamora@users.noreply.github.com>
2024-10-20 20:50:45 -0500

    feat: Agregar nueva página PageHome3 y router
    - Se creó un nuevo archivo `PageHome3.tsx` con contenido completo para la nueva sección.
    - Se actualizó `App.tsx` para incluir la ruta hacia `PageHome3`.
    - Se hicieron modificaciones en `index.css`, `colors.ts` y `tailwind.config.js` para ajustes de estilos y variables.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import PageGracias from "@/PageGracias.tsx";
 import PageHome from "@/PageHome.tsx";
 import PageHome2 from "@/PageHome2.tsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PageHome3 from "@/PageHome3.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: "/v2",
     element: <PageHome2 />,
+  },
+  {
+    path: "/v3",
+    element: <PageHome3 />,
   },
   {
     path: "/gracias",
--- src/shared/colors.ts
+++ src/shared/colors.ts
@@ -1,11 +1,3 @@
-// export const COLORS = {
-//   BACKGROUND: "#0A1A2A",
-//   TEXT: "#F5DEB3",
-//   PRIMARY: "#D4AF37",
-//   SECONDARY: "#40E0D0",
-//   BORDER: "#40E0D0",
-// };
-
 export const COLORS = {
   BACKGROUND: "#0A1A2A",
   TEXT: "#F5DEB3",
