b9a8f7e0fc0a4a46c6dbcc21a8f3fcd7f414ce52
luis david <11556208+luisdamora@users.noreply.github.com>
2024-10-20 20:09:28 -0500

    feat: Añadir nueva ruta para PageHome2 en App.tsx
    - Se agregó un nuevo componente PageHome2.
    - Se definió una nueva ruta para acceder a PageHome2 en la aplicación.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import PageGracias from "@/PageGracias.tsx";
 import PageHome from "@/PageHome.tsx";
+import PageHome2 from "@/PageHome2.tsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <PageHome />,
+  },
+  {
+    path: "/v2",
+    element: <PageHome2 />,
   },
   {
     path: "/gracias",
