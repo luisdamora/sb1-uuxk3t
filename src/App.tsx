f47955e133052eae1c1ba3e605d60341eacdc4a0
luis david <11556208+luisdamora@users.noreply.github.com>
2024-10-21 12:06:00 -0500

    refactor: Mejorar la estructura de App.tsx y PageHome3.tsx
    - Se reorganizó la importación de componentes en App.tsx.
    - Se comentaron las rutas relacionadas con PageHome2 y PageHome3.
    - Se cambiaron algunas etiquetas de <em> a <strong> para dar mayor énfasis en PageHome3.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import PageGracias from "@/PageGracias.tsx";
-import PageHome from "@/PageHome.tsx";
-import PageHome2 from "@/PageHome2.tsx";
 import PageHome3 from "@/PageHome3.tsx";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <PageHome />,
-  },
-  {
-    path: "/v2",
-    element: <PageHome2 />,
-  },
-  {
-    path: "/v3",
     element: <PageHome3 />,
   },
+  // {
+  //   path: "/v2",
+  //   element: <PageHome2 />,
+  // },
+  // {
+  //   path: "/v3",
+  //   element: <PageHome3 />,
+  // },
   {
     path: "/gracias",
     element: <PageGracias />,
